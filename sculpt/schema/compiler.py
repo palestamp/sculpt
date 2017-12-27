34ac4cb78adc9c6ae59df03b4e6b7eeb07813748
stas-kazhavets <stas.kazhavets@kufar.by>
2017-12-27 18:29:48 +0300

    Added missed compiler module

---
+++ sculpt/schema/compiler.py
@@ -0,0 +1,49 @@
+
+from sculpt.core import (
+    Copy, Switch, Input, Output, 
+    VirtualVar, Combine, Validate, Apply
+)
+
+from sculpt.validation import InSetValidator, NotEmptyValidator
+
+class Compiler(object):
+    fields = {
+        "input": Input,
+        "output": Output,
+        "virtual_var": VirtualVar,
+    }
+
+    actions = {
+        "validate": Validate,
+        "switch": Switch,
+        "copy": Copy,
+        "combine": Combine,
+        "apply": Apply,
+    }
+
+    validators = {
+        "InSet": InSetValidator,
+        "NotEmpty": NotEmptyValidator
+    }
+
+    def compile(self, rules):
+        return [self.load_operation(op) for op in rules]
+
+    def load_operation(self, op_spec):
+        op = op_spec["op"]
+        try:
+            cls = self.actions[op]
+        except KeyError:
+            raise Exception("unknown operation: {}".format(op))
+        return cls.compile(self, op_spec)
+
+    def load_field(self, field_spec):
+        field_type = field_spec["type"]
+        cls = self.fields[field_type]
+        return cls.compile(self, field_spec)
+
+    def load_validator(self, validator_spec):
+        validator_type = validator_spec["type"]
+        parameters = validator_spec.get("args", {})
+        cls = self.validators[validator_type]
+        return cls(**parameters)
